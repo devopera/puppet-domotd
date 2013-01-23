167ef460ad85ae1da8a36b433f44d748e4a95729
cleverlight <github.account@cleveraspaint.com>
2013-01-23 14:17:19 +0000

    first version shows basic information about this machine based on facter profile

---
+++ manifests/init.pp
@@ -0,0 +1,42 @@
+#
+# require concat
+#
+class domotd {
+   $motd = "/etc/motd"
+
+   concat{$motd:
+      owner => root,
+      group => root,
+      mode  => '0644',
+   }
+
+   # store current date and time
+   $date = inline_template('<%= Time.now.strftime("%l:%M %p on %Y-%m-%d") %>')
+
+   # create motd using general and custom facts
+   concat::fragment{"motd_header":
+      target => $motd,
+      content => "${::hostname} server, ${::processorcount} cores, ${::memorytotal} RAM, ${::operatingsystem} ${::operatingsystemrelease}, ${::environment} environment:\n${::fqdn} ${::ipaddress} [${::macaddress}]\nConfigured at ${date} with profile: ${::server_profile}\n",
+      order   => 01,
+   }
+
+   # local users on the machine can append to motd by just creating
+   # /etc/motd.local
+   concat::fragment{"motd_local":
+      target => $motd,
+      ensure  => "/etc/motd.local",
+      order   => 15
+   }
+}
+
+define domotd::register(
+  $content = $name,
+  $order = 10,
+) {
+  # add fragment to target file
+  concat::fragment{"motd_fragment_$name":
+    target  => '/etc/motd',
+    content => "$content",
+  }
+}
+
